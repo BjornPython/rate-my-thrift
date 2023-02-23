71e881038fddb7acbfd28eef3bb465c6bfc5f1ab
Nathan <nathanflores887@gmail.com>
2023-02-23 15:19:49 +0800

    Like button now works.

--- src/comps/loggedInPage/Post.jsx
+++ src/comps/loggedInPage/Post.jsx
@@ -1,16 +1,25 @@
 import "../../css/loggedIn/post.css"
-import demo from "../../images/demo.jpg"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHeart } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
 
 function Post({ title, caption, imgLink }) {
+
+    const [isLiked, setIsLiked] = useState(false)
+
+    const handleLike = () => {
+        setIsLiked(!isLiked)
+    }
+
     return (
         <div className="post">
-            <img src={imgLink} alt="pic" className="post-img" />
+            <img src={imgLink} alt="pic" className="post-img" onDoubleClick={handleLike} />
             <h1 className="post-title">{title}</h1>
             <div className="post-txt">
                 <p>{caption}</p>
-                <FontAwesomeIcon icon={faHeart} className="heart-icn" />
+                <FontAwesomeIcon icon={faHeart} className="heart-icn"
+                    style={isLiked ? { color: "#ef3a5d" } : {}}
+                    onClick={handleLike} />
             </div>
         </div>
     )
