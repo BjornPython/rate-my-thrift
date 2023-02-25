b2e6a4ebf231dc9d2b148953d2183dc5e55df01b
Nathan <nathanflores887@gmail.com>
2023-02-25 14:51:31 +0800

    Added image upload.

--- src/comps/loggedInPage/AddPost/AddPostPage.jsx
+++ src/comps/loggedInPage/AddPost/AddPostPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import { useState, useEffect } from 'react'
+import { uploadImage } from '../../../apis/firestireDataQueryFuncs'
 function AddPostPage() {
+
+    const [image, setImage] = useState([])
+    const [imageURL, setImageURL] = useState(null)
+
+    useEffect(() => {
+        if (image.length < 1) { return }
+        const url = URL.createObjectURL(image[0])
+        setImageURL(url)
+    }, [image])
+
     return (
         <div>
-            <input type="file" name="" id="" />
+            {imageURL && <img src={imageURL} alt="" style={{ width: "200px", height: "200px" }} />}
+            <div onClick={() => { setImageURL(null); setImage([]) }}>DELETE</div>
+            <input type="file" name="image" id="" onChange={(e) => { setImage(e.target.files) }} />
+
         </div>
     )
 }
