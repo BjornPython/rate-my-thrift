feaa946dcdcc7c3319b226fe30d73cc90d8a1ade
Nathan <nathanflores887@gmail.com>
2023-02-26 12:30:35 +0800

    Added a drag and drop file.

--- src/comps/loggedInPage/AddPost/AddPostPage.jsx
+++ src/comps/loggedInPage/AddPost/AddPostPage.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
 import { uploadImage } from '../../../apis/firestireDataQueryFuncs'
+import "../../../css/loggedIn/addpost/addPostPage.css"
+import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function AddPostPage() {
 
     const [image, setImage] = useState([])
@@ -18,11 +21,26 @@ function AddPostPage() {
     }
 
     return (
-        <div>
-            {imageURL && <img src={imageURL} alt="" style={{ width: "200px", height: "200px" }} />}
+        <div className='addpost-page'>
             <div onClick={() => { setImageURL(null); setImage([]) }}>DELETE</div>
-            <input type="file" name="image" id="" onChange={(e) => { setImage(e.target.files) }} />
-            <button onClick={handleImageUpload}>UPLOAD</button>
+
+            <div className="card">
+                {imageURL
+                    ?
+                    <img src={imageURL} alt="" className='image-prev' />
+                    :
+                    <span className='upload-here'>
+                        <FontAwesomeIcon className='upload-icn' icon={faArrowUpFromBracket} />
+                        <h3>drag and drop image here.</h3>
+                    </span>}
+                <input type="file" name="image" id="image" onChange={(e) => { setImage(e.target.files) }} className="input-file" />
+            </div>
+
+            <div className="upload-btns">
+                <label for="image" className='img-label'>Choose File</label>
+                <button onClick={handleImageUpload}>UPLOAD</button>
+            </div>
+
         </div>
     )
 }
