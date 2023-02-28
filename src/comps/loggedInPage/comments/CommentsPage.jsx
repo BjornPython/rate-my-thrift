381c8893b3d584716737255eae36ef5f1200e713
Nathan <nathanflores887@gmail.com>
2023-02-28 13:12:00 +0800

    Fixed like unnecessary rerendering of comments page.

--- src/comps/loggedInPage/comments/CommentsPage.jsx
+++ src/comps/loggedInPage/comments/CommentsPage.jsx
@@ -25,7 +25,7 @@ function CommentsPage({ uid, showCommentPage, commentPost, removeCommentsPage, c
 
         }
         userLikes()
-    })
+    }, [id])
 
     useEffect(() => {
         setLiked(isLiked)
