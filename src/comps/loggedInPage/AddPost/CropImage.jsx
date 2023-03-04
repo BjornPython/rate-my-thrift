04b1747bd7add4b856950f5d4cb71af5e1548cec
Nathan <nathanflores887@gmail.com>
2023-03-04 11:08:03 +0800

    FINALLY CNA NOW CROP IMAGES.

--- src/comps/loggedInPage/AddPost/CropImage.jsx
+++ src/comps/loggedInPage/AddPost/CropImage.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useCallback } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import Cropper from 'react-easy-crop'
+import { uploadImage } from '../../../apis/firestoreDataQueryFuncs'
 import getCroppedImg from '../../../cropping/cropImage'
 
 const img = "https://i.pinimg.com/564x/ad/bd/86/adbd86d3b3cc6fd37c9fc99c32cb7b27.jpg"
@@ -11,12 +12,20 @@ function CropImage() {
 
     const [croppedImage, setCroppedImage] = useState(null)
     const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+    const [uploadedImage, setUploadedImage] = useState(null)
+    const [imgURL, setImgURL] = useState(null)
+
+    useEffect(() => {
+        console.log("IMAGE: ", uploadedImage);
+        if (!uploadedImage) { return }
+        setImgURL(URL.createObjectURL(uploadedImage))
+    }, [uploadedImage])
 
 
     const showCroppedImage = useCallback(async () => {
         try {
             const croppedImage = await getCroppedImg(
-                img,
+                imgURL,
                 croppedAreaPixels
             )
             console.log('donee', { croppedImage })
@@ -41,14 +50,19 @@ function CropImage() {
     }
     return (
         <>
+            <input type="file" onChange={(e) => { setUploadedImage(e.target.files[0]) }} />
             <div className='cropimage-page'>
                 {/* <img src={img} alt="img preview" /> */}
                 <div className="crop-container"></div>
-                <Cropper image={img} zoom={zoom} crop={crop} aspect={aspect}
-                    onCropChange={onCropChange} onZoomChange={onZoomChange}
-                    onCropComplete={onCropComplete} />
+                {imgURL &&
+                    <>
+                        <Cropper image={imgURL} zoom={zoom} crop={crop} aspect={aspect}
+                            onCropChange={onCropChange} onZoomChange={onZoomChange}
+                            onCropComplete={onCropComplete} />
+                        <input type="range" className='zoom-range' value={zoom} min={1} max={3} step={0.1} onInput={(e) => { setZoom(e.target.value) }} />
+                    </>
+                }
 
-                <input type="range" className='zoom-range' value={zoom} min={1} max={3} step={0.1} onInput={(e) => { setZoom(e.target.value) }} />
             </div>
             <button onClick={showCroppedImage}>SHOW CROPPED IMAGE</button>
             {croppedImage && <img src='' alt='PREVIEW' />}
