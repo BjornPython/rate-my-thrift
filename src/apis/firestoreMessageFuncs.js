e1191e9034bd9106c11a632d8556e86f47859f63
Nathan <nathanflores887@gmail.com>
2023-03-15 09:26:04 +0800

    Added documentation for firebase functions.

--- src/apis/firestoreMessageFuncs.js
+++ src/apis/firestoreMessageFuncs.js
@@ -3,38 +3,28 @@ import { addDoc, arrayUnion, collection, doc, getDoc, getDocs, updateDoc, setDoc
 import { chatsCollection, userChatsCollection, usersCollection } from "./firebase";
 import {updated_at_timestamp} from "./firestoreDataQueryFuncs"
 
+// Creates a user chat, and adds the chatId to user's userChats.
 export const addUserChat = async (userId1, userId2) => {
-    console.log("ADDING USER...");
     try {
         const user1ChatsDocRef = doc(userChatsCollection, userId1, "userIds", userId2)
         const user1ChatsDoc = await getDoc(user1ChatsDocRef)
         if (user1ChatsDoc.exists()) {throw {message: "chat exists", chatId: user1ChatsDoc.data().chatId}}
         const chatId = await createChat(userId1, userId2);
-        console.log("CHAT ID: ", chatId);
-
         const userChats = await createUserChats(userId1, userId2, chatId);
     } catch (err) {throw err}
 }
 
-
+// updates the user's userChats.
 export const createUserChats = async (userId1, userId2, chatId) => {
-    console.log("RECEIVED: ", userId1, userId2, chatId);
     try {
         const user1ChatsRef = doc(userChatsCollection, userId1, "userIds", userId2)
-        console.log("SETTING NEW USER 1 CHAT");
         const newUser1Chat =  setDoc(user1ChatsRef, { chatId })
-
         const user2ChatsRef = doc(userChatsCollection, userId2, "userIds", userId1)
-        console.log("SETTING NEW USER 1 CHAT");
         const newUser2Chat =  setDoc(user2ChatsRef, { chatId })
-        
-    
     } catch(err) {throw err}
-    
-
 }
 
-
+// Creates the chat document.
 export const createChat = async (userId1, userId2) => {
     const chatDocRef = await addDoc(chatsCollection, {
         participants: [`${userId1}`, `${userId2}`],
@@ -45,25 +35,23 @@ export const createChat = async (userId1, userId2) => {
     return chatDocRef.id
 }
 
+// Creates a "messages" subcollection and adds a message document with a unique id.
 export const addMessage = async (chatId, senderId, content) => {
-    console.log("RECEIVED: ", chatId, senderId, content);
-
     const messageDocRef = collection(chatsCollection, chatId, "messages")
-    console.log("REF: ", messageDocRef);
     const messageDoc = await addDoc(messageDocRef, {
         content,
         senderId,
         dateTime: updated_at_timestamp
     })
-    console.log("MSGDOC RES: ", messageDocRef);
     if (messageDoc.id) {
         const chatsDoc = doc(chatsCollection, chatId)
         const updatedDoc = await updateDoc(chatsDoc, {lastMsg: content})
     }
-    console.log("RES: ", messageDoc.id);
     return messageDoc.id
 }   
 
+
+// Gets the messages of a chatDocument from it's "messages" subcollection.
 export const getMessages = async (chatId) => {
     const messageDocRef = collection(chatsCollection, chatId, "messages")
     const messageDocs = await getDocs(messageDocRef);
--- src/apis/firestoreUsersFuncs.js
+++ src/apis/firestoreUsersFuncs.js
@@ -2,56 +2,52 @@ import { usersCollection, notifCollection, userChatsCollection } from "./firebas
 import {  doc, setDoc, getDoc,addDoc } from "firebase/firestore";
 import { uploadImage } from "./firestoreDataQueryFuncs";
 
+// Initializes the user's firestore documents.
 export const addUser = async (uid, email) => {
-
-    const userDoc = doc(usersCollection, uid)  
-    const user = await getDoc(userDoc)
-    console.log("USER EXISTS? ", user.exists());
-    if (user.exists()) {return}
-    const newUser = await setDoc(userDoc, {
-        name: email,
-        dpURL: null,
-        email,
-        bio: "",
-        posts: []
-    })
-    const userNotifRef = doc(notifCollection, uid)
-    const newNotif = await setDoc(userNotifRef, {seen: true})
-    const userChatsRef =  doc(userChatsCollection, uid)
-    const userChats = await setDoc(userChatsRef, {})
-
-    console.log("NEW USER: ", newUser);
+    try {
+        const userDoc = doc(usersCollection, uid)  
+        const user = await getDoc(userDoc)
+        if (user.exists()) {return}
+        const newUser = await setDoc(userDoc, {
+            name: email,
+            dpURL: null,
+            email,
+            bio: "",
+            posts: []
+        })
+        const userNotifRef = doc(notifCollection, uid)
+        const newNotif = await setDoc(userNotifRef, {seen: true})
+        const userChatsRef =  doc(userChatsCollection, uid)
+        const userChats = await setDoc(userChatsRef, {})
+    } catch(err) {throw err}
 }   
 
+// Used for adding user's display picture.
 export const addDp = async (image, userId) => {
     try {
             const dpURL = await uploadImage(image, userId, false);
-    console.log("DP URL RES: ", dpURL);
-    const userDoc = doc(usersCollection, userId)
-    console.log("UPDATING USER DOC MERGE IS TRUE...");
-    const updatedUserDoc = await setDoc(userDoc, {dpURL}, { merge: true });
-    console.log("UPDATED DOC RES: ", updatedUserDoc);
-    return updatedUserDoc
+        const userDoc = doc(usersCollection, userId)
+        const updatedUserDoc = await setDoc(userDoc, {dpURL}, { merge: true });
+        return updatedUserDoc
     } catch (err) {throw err}
 }
 
-
+// Gets the user's info. used for getting dpURL, name, bio.
 export const getUserInfo = async (uid) => {
-    const userDoc = doc(usersCollection, uid)  
     try{
-            const document = await getDoc(userDoc);
-            return document.data()
+        const userDoc = doc(usersCollection, uid)  
+        const document = await getDoc(userDoc);
+        return document.data()
     } catch(err) {console.log(err);}
 
 }
 
 
     
-
+// Edits the user info, used for updating name, bio, dpURL
 export const editUserInfo = async (userId, newInfo) => {
-    const userDoc = doc(usersCollection, userId) 
-console.log("NEW INFO: ", newInfo);
     try {
+        const userDoc = doc(usersCollection, userId) 
         await setDoc(userDoc, newInfo, {merge: true})
         return true
     } catch(err) {console.log(err); throw err}
