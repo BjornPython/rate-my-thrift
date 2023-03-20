f05e2eb9b5bb1a6eb7990cf5702392233b947793
Nathan <nathanflores887@gmail.com>
2023-03-20 10:44:53 +0800

    Fixed messaging bug, need to fix rerouting for user authenticaiton next.

--- src/comps/loggedInPage/AddPost/AddPostTexts.jsx
+++ src/comps/loggedInPage/AddPost/AddPostTexts.jsx
@@ -5,7 +5,7 @@ function AddPostTexts({ title, caption, handleInputChange, changeUploadedImage, 
 
     return (
         <>
-            <input type="file" name="image" id="image" onChange={(e) => { changeUploadedImage(e.target.files[0]) }}
+            <input type="file" name="image" id="image" accept='image/*' onChange={(e) => { changeUploadedImage(e.target.files[0]) }}
                 className={`input-file ${uploadedImage && "off-input-file"}`} />
             <input type="text" name="title" value={title} className='upload-title' placeholder='Add a title' onChange={handleInputChange} />
             <input type="text" name="caption" value={caption} className='upload-caption' placeholder='Add a caption...' onChange={handleInputChange} />
